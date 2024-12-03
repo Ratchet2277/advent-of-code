715d286baa327d4f796c70be1b13c0832bc36c04
Jérémy Saudemont <jeremysaudemont@gmail.com>
2024-12-03 11:52:53 +0100

    Code cleanup

--- day-3/main.py
+++ day-3/main.py
@@ -1,7 +1,6 @@
-from typing import Dict
+import re
 
 import common
-import re
 
 if __name__ != '__main__':
     raise Exception('This file is not meant to be imported')
@@ -10,7 +9,7 @@ if __name__ != '__main__':
 def main():
     input_data = ''.join(common.read_file("input.txt"))
 
-    #Should be possible to do it in one regex, if you can't figure out how, I'm curious
+    # Should be possible to do it in one regex, if you can't figure out how, I'm curious
     mul_regex = re.compile(r"mul\((\d{1,3}),(\d{1,3})\)")
     do_regex = re.compile(r"do\(\)")
     dont_regex = re.compile(r"don't\(\)")
@@ -28,11 +27,11 @@ def main():
         max_do = list(filter(lambda x: x < mul.start(), do_index))
         max_dont = list(filter(lambda x: x < mul.start(), dont_index))
 
-        #Is there a "do()" match before this match
+        # Is there a "do()" match before this match
         if not max_do:
             continue
 
-        #Is there a "don't()" match, and determine which of "do()" and "don't()" is closer
+        # Is there a "don't()" match, and determine which of "do()" and "don't()" is closer
         if max_dont and max(max_do) < max(max_dont):
             continue
 
@@ -47,8 +46,6 @@ def get_all_match_index(regex: re, string: str) -> list[int]:
     for match in regex.finditer(string):
         output.append(match.start())
     return output
-
-
 
 
 main()
