32577452c302e82b29c8df05f6783e84be7b6bde
Jérémy Saudemont <jeremysaudemont@proton.me>
2025-06-04 20:25:45 +0200

    Add multi-threading and some optimization

--- day-11/main.py
+++ day-11/main.py
@@ -1,10 +1,11 @@
+import concurrent.futures
 from collections.abc import Callable
 
 import common
 import progressbar
 
 class Rule:
-    def __init__(self, condition: Callable[[int], bool], rule: Callable):
+    def __init__(self, condition: Callable[[int], bool], rule: Callable[[int], list[int]]):
         self.condition = condition
         self.rule = rule
 
@@ -17,22 +18,39 @@ def main():
         stones = blink(stones)
     print(len(stones))
 
-def get_rules() -> list[Rule]:
-    return [
-        Rule(lambda stone: stone == 0, lambda stone: [1]),
-        Rule(lambda stone: len(str(stone)) % 2 == 0,
-             lambda stone: [int(str(stone)[: len(str(stone)) // 2 ]), int(str(stone)[(len(str(stone)) // 2):])]),
-        Rule(lambda stone: True, lambda stone: [stone * 2024])
-    ]
+rules = [
+    Rule(lambda stone: not stone, lambda stone: [0]),
+    Rule(lambda stone: not bool(len(str(stone)) % 2),
+         lambda stone: split_stone(stone)),
+    Rule(lambda _: True, lambda stone: [stone * 2024])
+]
 
 def blink(stones: list[int]) -> list[int]:
-    new_stones: list[int] = []
+    new_stones = []
+    threads = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        batch_size = 200000
+        # calls change_stone_batch in parallel for each batch in stones
+        for i in range(0, len(stones), batch_size):
+            threads.append(executor.submit(change_stone_batch, stones[i:i + batch_size]))
+
+    for thread in threads:
+        new_stones.extend(thread.result())
+
+    return new_stones
+
+def split_stone(stone: int) -> list[int]:
+    stone = str(stone)
+    length = len(stone) >> 1
+    return [int(stone[:length]), int(stone[length:])]
+
+def change_stone_batch(stones: list[int]) -> list[int]:
+    new_stones = []
     for stone in stones:
         new_stones.extend(change_stone(stone))
     return new_stones
 
 def change_stone(stone: int) -> list[int]:
-    rules = get_rules()
     for rule in rules:
         if not rule.condition(stone):
             continue
