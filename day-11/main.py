67ab1949c6a78a8fe31086dc8e80e9c65403c787
Jérémy Saudemont <jeremysaudemont@proton.me>
2025-06-05 02:22:57 +0200

    At some point, I stop being stupid and start using dict + caching to avoid recalculating a billion times the same value, reducing hours of processing and terabytes of used memory to half a second runtime and few kilobytes

--- day-11/main.py
+++ day-11/main.py
@@ -1,5 +1,5 @@
 from collections.abc import Callable
-import shutil
+import common
 
 import progressbar
 
@@ -8,21 +8,24 @@ class Rule:
         self.condition = condition
         self.rule = rule
 
+cache: dict[int, list[int]] = {}
 
 def main():
-    shutil.copy('input.txt', 'last-result.txt')
-    for i in progressbar.progressbar(range(75)):
-        blink()
-        shutil.copy('output.txt', 'last-result.txt')
-    with open("last-result.txt", "r") as file:
-        count = 0
-        while True:
-            char = file.read(1)
-            if not char:
-                print(count)
-                return None
-            if char == " ":
-                count += 1
+    data = common.read_file("input.txt")[0]
+    data = [int(x) for x in data.split(" ")]
+    sorted_data: dict[int, int] = {}
+
+    for stone in data:
+        if stone not in sorted_data.keys():
+            sorted_data[stone] = 1
+            continue
+        sorted_data[stone] += 1
+
+    for _ in progressbar.progressbar(range(75)):
+        sorted_data = blink(sorted_data)
+    print(sum(sorted_data.values()))
+    print(len(sorted_data))
+
 rules = [
     Rule(lambda stone: not stone, lambda stone: [1]),
     Rule(lambda stone: not bool(len(str(stone)) % 2),
@@ -30,26 +33,15 @@ rules = [
     Rule(lambda _: True, lambda stone: [stone * 2024])
 ]
 
-def blink() -> None:
-    with open("last-result.txt", "r") as file, open("output.txt", "w") as output:
-        stone = ""
-        while True:
-            char = file.read(1)
-            if not char:
-                if not stone:
-                    return None
-                output.write(f"{" ".join([str(x) for x in change_stone(int(stone))])} ")
-                return None
-
-            if char == " ":
-                if not stone:
-                    continue
-                output.write(f"{" ".join([str(x) for x in change_stone(int(stone))])} ")
-                stone = ""
+def blink(stones: dict[int, int]) -> dict[int, int]:
+    new_stones = {}
+    for value in stones:
+        for stone in change_stone(value):
+            if stone not in new_stones.keys():
+                new_stones[stone] = stones[value]
                 continue
-
-            stone += char
-        return None
+            new_stones[stone] += stones[value]
+    return new_stones
 
 def split_stone(stone: int) -> list[int]:
     stone = str(stone)
@@ -57,10 +49,13 @@ def split_stone(stone: int) -> list[int]:
     return [int(stone[:length]), int(stone[length:])]
 
 def change_stone(stone: int) -> list[int]:
+    if stone in cache:
+        return cache[stone]
     for rule in rules:
         if not rule.condition(stone):
             continue
-        return rule.rule(stone)
+        cache[stone] = rule.rule(stone)
+        return cache[stone]
     raise ValueError("No rule found for this stone")
 
 if __name__ == '__main__':
